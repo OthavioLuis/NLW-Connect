f805639e5b493f36fe6806429cde57f1373b6dff
OthavioLuis <othaviorodriguesf@gmail.com>
2025-02-21 00:17:01 -0300

    Finalizando aula 3 com backend em Docker

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -1,13 +1,21 @@
 import Image from 'next/image';
 
-import logo from '../../assets/logo.svg';
+import logo from '../../../assets/logo.svg';
 import { Ranking } from './ranking';
 import { Stats } from './stats';
 import { InviteLinkInput } from './invite-link-input';
 
-export default function InvitePage() {
+interface InvitePageProps {
+    params: Promise<{
+        subscriberId: string
+    }>
+}
 
-    const inviteLink = 'https://localhost:3000/invite/19292'
+export default async function InvitePage(props: InvitePageProps) {
+
+    const {subscriberId} = await props.params
+
+    const inviteLink = `http://localhost:3333/invites/${subscriberId}`
 
     return (
         <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
@@ -31,7 +39,7 @@ export default function InvitePage() {
 
                     <InviteLinkInput inviteLink={inviteLink} />
 
-                    <Stats />
+                    <Stats subscriberId={subscriberId}/>
                 </div>
             </div>
 
