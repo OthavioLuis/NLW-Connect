423669021ea0bc07566ff568e51854a93cbc0c32
OthavioLuis <othaviorodriguesf@gmail.com>
2025-02-20 21:45:05 -0300

    Aula 2 - finalizando estilização e autenticando forms

--- src/components/iconButton.tsx
+++ src/components/iconButton.tsx
@@ -1,9 +1,12 @@
 import { ComponentProps, ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
 
 interface iconButtonProps extends ComponentProps<'button'> {}
 
-export function IconButton(props: iconButtonProps) {
+export function IconButton({ className, ...props}: iconButtonProps) {
     return (
-        <button className=" p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900" {...props}/>
+        <button className={twMerge(" p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900", className )}
+            {...props}
+        />
     )
 }
